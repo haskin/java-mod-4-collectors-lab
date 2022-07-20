360b1be8063aabe62098e4e773a31eae2ec05927
Adrian Khaskin <haskin.dev@gmail.com>
2022-07-20 14:41:30 -0400

    created implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BinaryOperator;
 import java.util.stream.Collectors;
 
 class Main {
 
     // modify this method
     public static long multiplySquaresOfNumbers(List<Integer> numbers) {
-        // your code here
-        return 0;
+        return numbers.stream().map(ele -> ele * ele)
+                .collect(Collectors.reducing((product, individual) -> product * individual)).get();
     }
 
     public static void main(String[] args) {
-
+        System.out.println("Input number values to get a product of all squared values : ");
         Scanner scanner = new Scanner(System.in);
 
         String[] values = scanner.nextLine().split(" ");
